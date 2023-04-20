b8196fc86b13765483588cba9de7646e1674bfd1
JIE <youco1327@yahoo.com.tw>
2023-04-20 23:47:24 +0800

    initial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <canvas
+      id="canvas"
+      width="500"
+      height="300"
+      style="border: 1px solid #000"
+    ></canvas>
+    <img class="show-img" alt="show-img" width="250" height="150" style="border: 1px solid" />
+    <div class="btn-group">
+      <button class="clear">Clear</button>
+      <button class="save">Save</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+
+  },
+};
+</script>
